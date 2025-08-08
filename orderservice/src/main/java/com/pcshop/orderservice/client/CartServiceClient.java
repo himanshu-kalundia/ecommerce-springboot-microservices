9deeee3722d64188a18012480a2f3134ab2de54d
programmerboi0987 <programerboi0987@gmail.com>
2025-08-08 16:55:59 +0530

    Eureka server is working now

--- orderservice/src/main/java/com/pcshop/orderservice/client/CartServiceClient.java
+++ orderservice/src/main/java/com/pcshop/orderservice/client/CartServiceClient.java
@@ -7,10 +7,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 
-@FeignClient(
-        name = "cartServiceClient",
-        url = "http://localhost:8083"
-)
+@FeignClient(name = "cart-service")
 public interface CartServiceClient {
 
     @GetMapping("/api/cart")
--- orderservice/src/main/java/com/pcshop/orderservice/client/UserServiceClient.java
+++ orderservice/src/main/java/com/pcshop/orderservice/client/UserServiceClient.java
@@ -5,10 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(
-        name = "userServiceClient",
-        url = "http://localhost:8081"
-)
+@FeignClient(name = "user-service")
 public interface UserServiceClient {
 
     @GetMapping("/api/user/me")
